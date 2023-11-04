ece49d12b43f6c15307fd5c47163f7144ee29104
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-11-04 04:37:19 +0000

    Update Rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *pivot, *temp;
 
-	if (!tree || !tree->right)
+	if (!tree || tree->right == NULL)
 		return (NULL);
 
 	pivot = tree->right;
